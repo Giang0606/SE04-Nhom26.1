3bf90bd861134562a21c29402be76e237ed73c18
Giang0606 <dieuly0606@gmail.com>
2020-11-18 02:51:16 +0700

    Xử lý data User, Message, Chatroom

--- server/server.js
+++ server/server.js
@@ -1,6 +1,26 @@
+require("dotenv").config();
+
+const mongoose = require("mongoose");
+mongoose.connect(process.env.DATABASE, {
+    useUnifiedTopology: true, 
+    useNewUrlParser: true
+});
+
+mongoose.connection.on('error', (err) => {
+    console.log("Mongoose connection error: "+ err.message);
+});
+
+mongoose.connection.once('open', () => {
+    console.log("MongoDB connected!");
+});
+
+require('./models/User');
+require('./models/Chatroom');
+require('./models/Message');
+
 const app = require('./app');
 const port = 8000;
 
 app.listen(port, () => {
     console.log("Server is listening on http://localhost:"+port);
-})
+});
